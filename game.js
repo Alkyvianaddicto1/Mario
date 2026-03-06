35776c0c7662c1544158146d1209e3e51b573df2
ZfluffySpicy <deofficialsklout254@gmail.com>
2026-03-06 16:42:58 +0300

    Speed identifiers

--- game.js
+++ game.js
@@ -5,3 +5,10 @@ kaboom({
   debug: true,
   clearColor: [0, 0, 0, 1],
 })
+
+const MOVE_SPEED = 120
+const JUMP_FORCE = 360
+const BIG_JUMP_FORCE = 550
+let CURRENT_JUMP_FORCE = JUMP_FORCE
+const FALL_DEATH = 400
+const ENEMY_SPEED = 20
